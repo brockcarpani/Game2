496880b8865e5a5df5c0b7bd57309c70bbaac564
Brock Carpani <brockcarpani@users.campus>
2020-02-19 16:47:08 -0600

    Add platforms

---
+++ MonoGameWindowsStarter/Platform.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+
+namespace MonoGameWindowsStarter
+{
+    /// <summary>
+    /// A class representing a platform
+    /// </summary>
+    public class Platform : IBoundable
+    {
+        /// <summary>
+        /// The platform's bounds
+        /// </summary>
+        BoundingRectangle bounds;
+
+        /// <summary>
+        /// The platform's sprite
+        /// </summary>
+        Sprite sprite;
+
+        /// <summary>
+        /// The number of times the sprite is repeated (tiled) in the platform
+        /// </summary>
+        int tileCount;
+
+        /// <summary>
+        /// The bounding rectangle of the 
+        /// </summary>
+        public BoundingRectangle Bounds => bounds;
+
+        /// <summary>
+        /// Constructs a new platform
+        /// </summary>
+        /// <param name="bounds">The platform's bounds</param>
+        /// <param name="sprite">The platform's sprite</param>
+        public Platform(BoundingRectangle bounds, Sprite sprite)
+        {
+            this.bounds = bounds;
+            this.sprite = sprite;
+            tileCount = (int)bounds.Width / sprite.Width;
+        }
+
+        /// <summary>
+        /// Draws the platform
+        /// </summary>
+        /// <param name="spriteBatch">The spriteBatch to render to</param>
+        public void Draw(SpriteBatch spriteBatch)
+        {
+            for (int i = 0; i < tileCount; i++)
+            {
+                sprite.Draw(spriteBatch, new Vector2(bounds.X + i * sprite.Width, bounds.Y), Color.White);
+            }
+
+        }
+    }
+}
